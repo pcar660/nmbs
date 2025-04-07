6552ff8a74e9b14e68d68305bea18b02e57c1193
pcar660 <pcar660@gmail.com>
2025-04-07 13:52:21 +0200

    Update create_RIV_WORK_datasets.sql

--- create_RIV_WORK_datasets.sql
+++ create_RIV_WORK_datasets.sql
@@ -1,7 +1,7 @@
-CREATE TABLE audience_riv_work_20250407_4 (
-    hashed_email_id TEXT PRIMARY identity NAMESPACE 'hashedEmailId',
-    riv_work_segment ROW (
-        is_in_segment BOOLEAN,
-        segment_created_datetime datetime
-    )
-) WITH (LABEL='PROFILE');
+        CREATE TABLE audience_riv_work_20250407_1 (
+            hashed_email_id TEXT PRIMARY identity NAMESPACE 'hashedEmailId',
+            riv_work_segment ROW (
+                is_in_segment BOOLEAN,
+                segment_created_datetime datetime
+            )
+        ) WITH (LABEL='PROFILE');
